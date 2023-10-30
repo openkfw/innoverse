12974d3b795d8b8a74fadf79f694b786e5fb681e
Mayr, Martin <22145802+mayrmartin@users.noreply.github.com>
2023-10-30 15:44:12 +0100

    add enviroment context & include umami on-demand

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -19,10 +19,12 @@ const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <Script
-        src={`${process.env.NEXT_PUBLIC_UMAMI_URL}/script.js`}
-        data-website-id={`${process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}`}
-      />
+      {process.env.NEXT_PUBLIC_UMAMI_URL && process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID && (
+        <Script
+          src={`${process.env.NEXT_PUBLIC_UMAMI_URL}/script.js`}
+          data-website-id={`${process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}`}
+        />
+      )}
       <body>
         <SessionProvider>
           <SWRProvider>
