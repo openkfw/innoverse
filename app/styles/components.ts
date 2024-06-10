0559d6d573a68fc2f373e5e935f782ef17701844
Blaas Nadine <122269231+naadnn@users.noreply.github.com>
2024-06-10 17:45:21 +0200

    lighthouse errors fixed

--- app/styles/components.ts
+++ app/styles/components.ts
@@ -15,54 +15,63 @@ const components: Components<Omit<Theme, 'components'>> = {
 
       @font-face {
         font-family: '***FONT_REMOVED***';
+        font-display: optional;
         src: local('***FONT_REMOVED***) format('woff2');
         unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
       }
 
       @font-face {
         font-family: '***FONT_REMOVED***';
+        font-display: optional;
         src: local('***FONT_REMOVED***) format('woff2');
         unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
       }
 
       @font-face {
         font-family: '***FONT_REMOVED***';
+        font-display: optional;
         src: local('***FONT_REMOVED***) format('woff2');
         unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
       }
 
       @font-face {
         font-family: '***FONT_REMOVED***';
+        font-display: optional;
         src: local('***FONT_REMOVED***) format('woff2');
         unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
       }
 
       @font-face {
         font-family: '***FONT_REMOVED***';
+        font-display: optional;
         src: local('***FONT_REMOVED***) format('woff2');
         unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
       }
 
       @font-face {
         font-family: '***FONT_REMOVED***';
+        font-display: optional;
         src: local('***FONT_REMOVED***) format('woff2');
         unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
       }
 
       @font-face {
         font-family: 'PFCentroSansProReg';
+        font-display: optional;
         src: local('PFCentroSansProReg'), url(/fonts/PFCentroSansPro-Reg.woff2) format('woff2');
         unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
       }
 
       @font-face {
         font-family: 'PFCentroSansProMed';
+        font-display: optional;
         src: local('PFCentroSansProMed'), url(/fonts/PFCentroSansPro-Med.woff2) format('woff2');
         unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
       }
 
       @font-face {
         font-family: 'PFCentroSansProBold';
+        font-display: optional;
         src: local('PFCentroSansProBold'), url(/fonts/PFCentroSansPro-Bold.woff2) format('woff2');
         unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
       }
