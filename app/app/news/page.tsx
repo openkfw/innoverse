6d4eb3400ecc8d77055d321e01d2313816d8b142
Blaas Nadine <122269231+naadnn@users.noreply.github.com>
2024-06-12 15:10:31 +0200

    component and style updates

--- app/app/news/page.tsx
+++ app/app/news/page.tsx
@@ -1,18 +1,16 @@
 import Image from 'next/image';
 
-import RefreshOutlinedIcon from '@mui/icons-material/RefreshOutlined';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
-import Icon from '@mui/material/Icon';
-import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 
 import BreadcrumbsNav from '@/components/common/BreadcrumbsNav';
 import ErrorPage from '@/components/error/ErrorPage';
 import NewsContainer from '@/components/newsPage/NewsContainer';
+import ResetBanner from '@/components/newsPage/resetBanner/ResetBanner';
 import { getProjectUpdates, getProjectUpdatesPage } from '@/utils/requests/updates/requests';
 
 import { NewsFilterContextProvider } from '../contexts/news-filter-context';
@@ -44,31 +42,6 @@ async function NewsPage() {
           <BreadcrumbsNav activePage="News" />
         </Box>
         <Grid container sx={containerStyles}>
-
-
-        <Card sx={cardStylesUpdate}>
-            <Icon
-              style={{
-                color: 'white',
-                width: '16',
-                height: '16',
-              }}><RefreshOutlinedIcon /></Icon>
-            <Typography variant='subtitle1'>7 neue Updates verfügbar</Typography>
-            <Link 
-            style={{
-              color: '#B7F9AA',
-              textDecoration: 'underline',
-            }}
-            /*
-            onClick={() => {
-              console.log('Link clicked!');
-            }}
-              */
-            >Jetzt zeigen</Link>
-        </Card>
-            
-
-
           <Card sx={cardStyles}>
             <Typography variant="h2" sx={cardTitleStyles}>
               News
@@ -82,7 +55,7 @@ async function NewsPage() {
           <NewsContainer news={initialUpdates} />
         </NewsFilterContextProvider>
       </Container>
-    </Stack>
+    </Stack>    
   );
 }
 
@@ -97,22 +70,6 @@ const cardStyles = {
   height: 200,
   ml: { lg: -4, md: -4, sm: 0 },
 };
-
-const cardStylesUpdate = {
-  display: 'flex',
-  padding: '24px 16px',
-  justifyContent: 'center',
-  alignItems: 'center',
-  gap: '8px',
-  borderRadius: '16px',
-  border: '1px solid rgba(183, 249, 170, 0.80)',
-  backgroundColor: 'rgba(255, 255, 255, 0.20)',
-  boxShadow: '0px 12px 40px 0px rgba(0, 0, 0, 0.25)',
-  backdropFilter: 'blur(20px)',
-  width: 761,
-  height: 76,
-};
-
 
 const cardTitleStyles = {
   fontSize: '48px',
