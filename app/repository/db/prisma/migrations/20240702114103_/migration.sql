76956ed03fd084d6d5fe5eade3b6603ee31742b0
Blaas Nadine <122269231+naadnn@users.noreply.github.com>
2024-07-03 15:37:17 +0200

    Update migration.sql

--- app/repository/db/prisma/migrations/20240702114103_/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "collaboration_comments" ALTER COLUMN "updatedAt" DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE "collaboration_comments_responses" ALTER COLUMN "updatedAt" DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE "project_comments" ALTER COLUMN "updatedAt" DROP DEFAULT;
