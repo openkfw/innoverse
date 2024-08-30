0d6118acf69f352b556ff48c1ca02153d58af99d
Fiedorowicz, Samuel <56935464+expries@users.noreply.github.com>
2024-08-30 19:46:48 +0200

    style: Remove unnecessary comments

--- .versionrc.js
+++ .versionrc.js
@@ -12,16 +12,16 @@ module.exports = {
   packageFiles: [nextPackageFile, strapiPackageFile],
   bumpFiles: [nextPackageFile, strapiPackageFile],
   types: [
-    { type: "build", hidden: true }, // Build system changes will be hidden in the CHANGELOG
-    { type: "chore", hidden: true }, // Chores will be hidden in the CHANGELOG
-    { type: "pipeline", hidden: true }, // CI-related changes will be hidden in the CHANGELOG
-    { type: "docs", section: "Documentation" }, // Documentation changes will show up under "Documentation"
-    { type: "feat", section: "Features" }, // Features will show up under "Features" in the CHANGELOG
-    { type: "fix", section: "Bug Fixes" }, // Bug Fixes will show up under "Bug Fixes" in the CHANGELOG
-    { type: "perf", section: "Performance Improvements" }, // Performance Improvements will have their own section
-    { type: "refactor", hidden: true }, // Refactoring will have their own section
-    { type: "style", hidden: true }, // Style changes will be hidden in the CHANGELOG
-    { type: "test", hidden: true }, // Test-related changes will be hidden in the CHANGELOG
-    { type: "breaking", section: "Breaking Changes" }, // Breaking changes will have their own section
+    { type: "build", hidden: true },
+    { type: "chore", hidden: true },
+    { type: "pipeline", hidden: true },
+    { type: "docs", section: "Documentation" },
+    { type: "feat", section: "Features" },
+    { type: "fix", section: "Bug Fixes" },
+    { type: "perf", section: "Performance Improvements" },
+    { type: "refactor", hidden: true },
+    { type: "style", hidden: true },
+    { type: "test", hidden: true },
+    { type: "breaking", section: "Breaking Changes" },
   ],
 };
