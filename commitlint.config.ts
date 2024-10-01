de554660abaa3c84fdd18b5083d5b36e78400ea3
expries <56935464+expries@users.noreply.github.com>
2024-10-01 08:48:15 +0200

    docs: update release process (#72)
    * chore: commit subject must be lowercase
    
    * chore: add pipeline section to changelog
    
    * docs: describe release process in more detail
    
    * docs: describe release process in more detail
    
    * chore(release): 1.0.2
    
    * docs: describe release process in more detail
    
    * docs: describe release process in more detail
    
    * chore: remove test changelog from changelog.md
    
    * chore: remove debug version bump

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -10,7 +10,7 @@ const Configuration = {
     // Scope must be lowercase
     "scope-case": [2, "always", ["lower-case"]],
     // Subject may have any case
-    "subject-case": [0],
+    "subject-case": [2, "always", ["lower-case"]],
     // Type must be in the following list
     "type-enum": [
       2, // Causes an error on violation
