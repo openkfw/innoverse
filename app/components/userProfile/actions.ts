d289ff9026b3b9baa2dbe8809f5c3f7b00c3d3ce
andrea.smiesna <122895253+andrea-smiesna@users.noreply.github.com>
2024-12-02 08:48:04 +0100

    feat: add user profile page, add form

---
+++ app/components/userProfile/actions.ts
@@ -0,0 +1,59 @@
+'use server';
+
+import { StatusCodes } from 'http-status-codes';
+
+import { UserSession } from '@/common/types';
+import { RequestError } from '@/entities/error';
+import { withAuth } from '@/utils/auth';
+import { InnoPlatformError, strapiError } from '@/utils/errors';
+import getLogger from '@/utils/logger';
+import { getInnoUserByProviderId, updateInnoUser } from '@/utils/requests/innoUsers/requests';
+import { validateParams } from '@/utils/validationHelper';
+
+import { handleUpdateUserSession } from './validationSchema';
+
+const logger = getLogger();
+
+export const updateUserProfile = withAuth(
+  async (user: Omit<UserSession, 'image'>, body: Omit<UserSession, 'image'> & { image: FormData }) => {
+    const validatedParams = validateParams(handleUpdateUserSession, body);
+
+    if (validatedParams.status !== StatusCodes.OK) {
+      return {
+        status: validatedParams.status,
+        errors: validatedParams.errors,
+        message: validatedParams.message,
+      };
+    }
+
+    const createInnoPlatformError = (errorMessage: string) => {
+      const error: InnoPlatformError = strapiError(`Updating user profile by user ${user.providerId}`, {
+        info: errorMessage,
+      } as RequestError);
+      return error;
+    };
+
+    const author = await getInnoUserByProviderId(user.providerId);
+
+    if (!author) {
+      const error = createInnoPlatformError('InnoUser does not exist');
+      logger.error(error);
+      return {
+        status: StatusCodes.INTERNAL_SERVER_ERROR,
+        errors: 'Updating an Inno User failed',
+      };
+    }
+
+    const updatedUser = await updateInnoUser({
+      ...user,
+      // TODO: fix any
+      ...(body as any),
+      id: author.id as string,
+    });
+
+    return {
+      status: StatusCodes.OK,
+      data: updatedUser,
+    };
+  },
+);
--- app/components/userProfile/formFields.tsx
+++ app/components/userProfile/formFields.tsx
@@ -1,7 +1,12 @@
 const formFieldNames = {
+  NAME: 'name',
   EMAIL: 'email',
   USERNAME: 'username',
-  ANONYMOUS: 'anonymous',
+  ROLE: 'role',
+  DEPARTMENT: 'department',
+  IMAGE: 'image',
+  EMAIL_NOTIFICATIONS: 'email_notifications',
+  PUSH_NOTIFICATIONS: 'push_notifications',
 };
 
 export default formFieldNames;
