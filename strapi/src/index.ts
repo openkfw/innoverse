5441c0079b22739383cf92a2efee24483443583c
andrea.smiesna <122895253+andrea-smiesna@users.noreply.github.com>
2025-01-24 17:39:45 +0100

    fix: fix the types in object mappings

--- strapi/src/index.ts
+++ strapi/src/index.ts
@@ -23,35 +23,41 @@ export default {
             const { id, participantId } = args;
             return await strapi.db.transaction(
               async ({ trx, rollback, commit, onCommit, onRollback }) => {
-                const entries = await strapi.documents("api::opportunity.opportunity").findOne({
-                  documentId: id,
-                  fields: ["documentId", "title"],
-                  populate: { participants: { fields: ["documentId"] } }
-                });
+                const entries = await strapi
+                  .documents("api::opportunity.opportunity")
+                  .findOne({
+                    documentId: id,
+                    fields: ["documentId", "title"],
+                    populate: { participants: { fields: ["documentId"] } },
+                  });
                 const participantIds = entries.participants.map(
-                  (participant) => participant.id
+                  (participant) => participant.documentId
                 );
 
                 // If participant is already saved in list of participants, remove the participandId from the array
                 if (participantIds.includes(parseInt(participantId))) {
-                  const updateRequest = await strapi.documents("api::opportunity.opportunity").update({
-                    documentId: id,
-                    data: {
-                      participants: participantIds.filter((id) => {
-                        return id != participantId;
-                      }),
-                    }
-                  });
+                  const updateRequest = await strapi
+                    .documents("api::opportunity.opportunity")
+                    .update({
+                      documentId: id,
+                      data: {
+                        participants: participantIds.filter((id) => {
+                          return id != participantId;
+                        }),
+                      },
+                    });
                   return toEntityResponse(updateRequest);
                 }
 
                 // If participant is not saved in list of participants, add the participandId to the array
-                const updateRequest = await strapi.documents("api::opportunity.opportunity").update({
-                  documentId: id,
-                  data: {
-                    participants: [participantId, ...participantIds],
-                  }
-                });
+                const updateRequest = await strapi
+                  .documents("api::opportunity.opportunity")
+                  .update({
+                    documentId: id,
+                    data: {
+                      participants: [participantId, ...participantIds],
+                    },
+                  });
 
                 return toEntityResponse(updateRequest);
               }
