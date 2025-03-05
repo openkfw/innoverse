9ee2bd8553144da622dceee5034bd6a069c8c222
pecirep <pecirep@users.noreply.github.com>
2025-03-05 16:28:44 +0100

    feat: store email content in strapi
    Signed-off-by: pecirep <pecirep@users.noreply.github.com>

---
+++ strapi/src/api/weekly-email-template/controllers/weekly-email-template.ts
@@ -0,0 +1,7 @@
+/**
+ * weekly-email-template controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::weekly-email-template.weekly-email-template');
