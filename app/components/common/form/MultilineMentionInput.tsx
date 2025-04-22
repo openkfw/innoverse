47c2eb915a2adffb5b4eaf2b3b956ea0b28b7a57
andrea.smiesna <122895253+andrea-smiesna@users.noreply.github.com>
2025-04-22 16:50:47 +0200

    fix: adjust the style

--- app/components/common/form/MultilineMentionInput.tsx
+++ app/components/common/form/MultilineMentionInput.tsx
@@ -103,7 +103,6 @@ const mentionInputStyle = {
     position: 'relative',
   },
   highlightedInput: {
-    backgroundColor: 'rgba(250,249,246,255)',
     color: '#398357',
     zIndex: 1,
     position: 'relative',
