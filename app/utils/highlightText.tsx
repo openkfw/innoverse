afa9683d2c7300440217362246ef293c36882cae
andrea.smiesna <122895253+andrea-smiesna@users.noreply.github.com>
2025-06-03 17:21:38 +0200

    fix: escape regex expression

--- app/utils/highlightText.tsx
+++ app/utils/highlightText.tsx
@@ -1,24 +1,30 @@
 import React, { useMemo } from 'react';
-
 import { useHighlightContext } from '@/app/contexts/news-feed-highlight-context';
+import { escapeRegexExp } from './helpers';
 
 export function HighlightText({ text }: { text: string | undefined }) {
   const { highlightString } = useHighlightContext();
-  const regex = useMemo(() => new RegExp(`(${highlightString})`, 'gi'), [highlightString]);
 
-  const renderText = (text: string | undefined) => {
-    if (!text) return;
-    const parts = text.split(regex);
-    return parts.map((part, index) =>
-      part.toLowerCase() === highlightString?.toLowerCase() ? (
-        <span key={index} style={{ backgroundColor: '#FFE95E' }}>
-          {part}
-        </span>
-      ) : (
-        part
-      ),
-    );
-  };
+  const regex = useMemo(() => {
+    if (!highlightString) return null;
+    const escapedString = escapeRegexExp(highlightString);
+    return new RegExp(`(${escapedString})`, 'gi');
+  }, [highlightString]);
 
-  return <>{renderText(text)}</>;
+  if (!text) return null;
+  const parts = regex ? text.split(regex) : [text];
+
+  return (
+    <>
+      {parts.map((part, index) =>
+        part.toLowerCase() === highlightString?.toLowerCase() ? (
+          <span key={index} style={{ backgroundColor: '#FFE95E' }}>
+            {part}
+          </span>
+        ) : (
+          part
+        ),
+      )}
+    </>
+  );
 }
