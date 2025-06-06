5af5613077f128e12f2d032c1d474cbb455f75f7
pecirep <pecirep@users.noreply.github.com>
2025-06-06 10:45:17 +0200

    fix: only trigger weekly email in dev env or from cron job
    Signed-off-by: pecirep <pecirep@users.noreply.github.com>

--- app/app/api/hooks/weekly-email/route.ts
+++ app/app/api/hooks/weekly-email/route.ts
@@ -5,13 +5,16 @@ import { ReasonPhrases, StatusCodes } from 'http-status-codes';
 import { serverConfig } from '@/config/server';
 import { sendWeeklyEmail } from '@/utils/emails/weeklyEmail';
 
-export async function POST() {
+export async function POST(request: Request) {
   const headersList = await headers();
   const authorization = headersList.get('authorization');
 
   if (authorization == serverConfig.STRAPI_PUSH_NOTIFICATION_SECRET) {
-    after(sendWeeklyEmail);
-    return new Response(ReasonPhrases.OK);
+    const { event } = await request.json();
+    if (event === 'trigger-cron' || process.env.NODE_ENV === 'development') {
+      after(sendWeeklyEmail);
+      return new Response(ReasonPhrases.OK);
+    } else return new Response(ReasonPhrases.BAD_REQUEST, { status: StatusCodes.BAD_REQUEST });
   } else {
     return new Response(ReasonPhrases.UNAUTHORIZED, { status: StatusCodes.UNAUTHORIZED });
   }
